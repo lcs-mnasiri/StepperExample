a857d05feb48dbbb3b833f7c75b5d0a9436c640d
Machid Nasiri <mnasiri@lcs.on.ca>
2026-04-21 09:05:17 -0400

    Initial Commit

---
+++ StepperExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StepperExample
+//
+//  Created by Machid on 4/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Machid on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
